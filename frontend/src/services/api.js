9ef62411b2b3099f00a8ea1eaa4868542d28e813
Rogério Soares <rgrsoares@yahoo.com.br>
2025-02-18 13:20:32 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
     //MODE já existe por default no Vite
-    baseURL: import.meta.env.MODE === 'production'
+   /*baseURL: import.meta.env.MODE === 'production'
         ? import.meta.env.RENDER_BACKEND_URL
-        : import.meta.env.VITE_API_URL
+        : import.meta.env.VITE_API_URL*/
+    baseURL: 'https://my-shops-with-react-backend.onrender.com/api'
 });
 
 export default api;
