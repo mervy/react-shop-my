a069712b16442152d1463356258858829f8d8c1a
Rogério Soares <rgrsoares@yahoo.com.br>
2025-02-27 16:29:12 -0300

    Import component teste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Teste from './components/Teste';
 import Login from './components/Login';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
