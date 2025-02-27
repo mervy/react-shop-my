cc7cae29ca5453fdcca8d65340ce22353c855f60
Rogério Soares <rgrsoares@yahoo.com.br>
2025-02-27 16:14:52 -0300

    Alter api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,10 +4,7 @@ const api = axios.create({
     //MODE já existe por default no Vite
     baseURL: import.meta.env.MODE === 'production'
         ? import.meta.env.RENDER_BACKEND_URL
-        : import.meta.env.VITE_API_URL,
-        headers: {
-           'Content-Type': 'application/json', 
-        }
+        : import.meta.env.VITE_API_URL       
 });
 
 export default api;
