b598cb5f2a9a589d5af076313b629fbf1633992a
Rogério Soares <rgrsoares@yahoo.com.br>
2025-02-27 15:59:14 -0300

    Add route login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/Login';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import Navbar from './components/Navbar';
